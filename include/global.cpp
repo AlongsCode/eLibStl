ced9014e9bffbd537d4f504daaf25abd97918ed5
AlongsCode <112497370+AlongsCode@users.noreply.github.com>
2023-01-16 14:42:51 +0800

    Delete global.cpp

--- include/global.cpp
+++
@@ -1,6 +0,0 @@
-//#include<vector>
-//#include"elib/lib2.h"
-//std::vector<PFN_EXECUTE_CMD> s_ExecuteCommand;
-//std::vector<const char*> s_CommandNames;
-//std::vector<CMD_INFO> s_Commands;
-//std::vector<LIB_DATA_TYPE_INFO> g_DataType;
